cc690430e85cacc3c4f1051451c62ab4e20d5a4c
robond <fouliex@gmail.com>
2017-06-05 21:21:08 -0700

    add simple_mover

--- build/first_package/CTestTestfile.cmake
+++ build/first_package/CTestTestfile.cmake
@@ -1,9 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/robond/catkin_ws/src
-# Build directory: /home/robond/catkin_ws/build
+# Source directory: /home/robond/catkin_ws/src/first_package
+# Build directory: /home/robond/catkin_ws/build/first_package
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(first_package)
-subdirs(simple_arm)
